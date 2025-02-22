905e2eccfc70677699dad289e062cbe26c69a129
Fleefie <anon>
2025-02-22 17:28:24 +0100

    Project structure

---
+++ src/main/java/fr/cytech/projet_devweb_ing1/ServletInitializer.java
@@ -0,0 +1,13 @@
+package fr.cytech.projet_devweb_ing1;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProjetDevwebIng1Application.class);
+	}
+
+}
