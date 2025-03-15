b9d5a589e7b0b9e1c44ab5d0dcbd5437c35c8629
Fleefie <anon>
2025-03-15 16:24:00 +0100

    Typo 2

--- application/src/main/resources/db/migration/V1__init.sql
+++ application/src/main/resources/db/migration/V1__init.sql
@@ -4,7 +4,7 @@ CREATE TABLE users (
     username TEXT UNIQUE NOT NULL,
     email TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
-    verified BOOLEAN NOT NULL,
+    verified BOOLEAN NOT NULL
 );
 
 CREATE TABLE roles (
